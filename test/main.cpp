6ecbfbc58548de0b807468d5a9d1da5adee539be
chxuan <787280310@qq.com>
2016-07-08 17:46:20 +0800

    Update smartdb

--- test/main.cpp
+++ test/main.cpp
@@ -22,6 +22,26 @@ void testJsonUtil()
     std::cout << "testJsonUtil time: " << t.elapsed() << "ms" << std::endl;
 }
 
+using namespace std;
+void testCreateTable()
+{
+    smartdb::Database db;
+	db.open("test.db");
+
+	const string sqlcreat = "CREATE TABLE if not exists PersonTable(ID INTEGER NOT NULL, Name Text, Address BLOB);";
+	if (!db.Excecute(sqlcreat))
+		return;
+
+	const string sqlinsert = "INSERT INTO PersonTable(ID, Name, Address) VALUES(?, ?, ?);";
+	int id = 2;
+	string name = "Peter";
+	string city = "zhuhai";
+	blob bl = { city.c_str(), city.length() + 1 };
+	if (!db.Excecute(sqlinsert, id, "Peter", nullptr))
+		return;
+
+}
+
 int main()
 {
     smartdb::Database db;
