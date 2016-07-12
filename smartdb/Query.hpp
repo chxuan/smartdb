1690249fe1a2f892dc9eed43adcc712bcf10c2dd
chxuan <787280310@qq.com>
2016-07-12 20:50:30 +0800

    Update smartdb

--- smartdb/Query.hpp
+++ smartdb/Query.hpp
@@ -22,7 +22,7 @@ public:
         return m_colCount == 0 ? false : true;
     }
     
-    void readTable(sqlite3_stmt* statement)
+    bool readTable(sqlite3_stmt* statement)
     {
         m_buf.clear();
         while (true)
@@ -33,27 +33,73 @@ public:
                 break;
             }
 
-            readRow(statement);
+            if (!readRow(statement))
+            {
+                sqlite3_reset(statement);
+                m_buf.clear();
+                return false;
+            }
         }
 
         sqlite3_reset(statement);
+        return true;
     }
 
 private:
-    void readRow(sqlite3_stmt* statement)
+    bool readRow(sqlite3_stmt* statement)
     {
         std::vector<DBVariant> rowBuf;
         rowBuf.reserve(m_colCount);
         for (int i = 0; i < m_colCount; ++i)
         {
-            readValue(statement, i, rowBuf);
+            if (!readValue(statement, i, rowBuf))
+            {
+                return false;
+            }
         }
+
         m_buf.emplace_back(std::move(rowBuf));
+        return true;
     }
 
     bool readValue(sqlite3_stmt* statement, int index, std::vector<DBVariant>& buf)
     {
         int type = sqlite3_column_type(statement, index);
+        switch (type)
+        {
+        case SQLITE_INTEGER: 
+            {
+                buf.emplace_back(sqlite3_column_int64(statement, index));
+                break;
+            }
+        case SQLITE_FLOAT: 
+            {
+                buf.emplace_back(sqlite3_column_double(statement, index));
+                break; 
+            }
+        case SQLITE_TEXT:
+            {
+                buf.emplace_back(std::string(reinterpret_cast<const char*>(sqlite3_column_text(statement, index))));
+                break; 
+            }
+        case SQLITE_BLOB: 
+            {
+                buf.emplace_back(std::string(reinterpret_cast<const char*>(sqlite3_column_blob(statement, index)), 
+                                             sqlite3_column_bytes(statement, index)));
+                break;
+            }
+        case SQLITE_NULL:
+            {
+                buf.emplace_back(nullptr);
+                break; 
+            }
+        default: 
+            {
+                std::cout << "Invaild type: " << type << std::endl;
+                return false;
+            }
+        }
+
         return true;
     }
 
@@ -61,7 +107,6 @@ private:
     std::vector<std::vector<DBVariant>> m_buf;
     int m_code = 0;
     int m_colCount = 0;
-    /* static std::unordered_map<int, std::function<void() */
 };
 
 };
